7d4665c647c8a9e74b95cac67a297e4e4b15a383
Nolan Kantner <94024607+nolankantner@users.noreply.github.com>
2026-01-18 18:40:11 -0800

    Update AdditionWorkBoardCoinAmount.py

--- python/AdditionWorkBoardCoinAmount.py
+++ python/AdditionWorkBoardCoinAmount.py
@@ -1,3 +1,5 @@
+#This is a script to help find how many coins is needed for 1 work board set
+
 coins = [0,0,0,0,0,0,0,0,0,0] #index counts as coin number
 for x in range(0,1000): #change 1000 in lines 2 and 3 for higher digit boards (100 for tens place, 10000 for thousands place, etc.)
     for y in range(0,1000):
@@ -6,4 +8,5 @@ for x in range(0,1000): #change 1000 in lines 2 and 3 for higher digit boards (1
                 coins[i] = str(x).count(str(i)) + str(y).count(str(i)) + str(x+y).count(str(i))
 coins[1] += 2 #adds 2 extra 1 coins for the 2 sliders
 for i in range(0,10):
+
     print("Coin",str(i),"  Amount",coins[i])
