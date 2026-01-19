5b03ab4abc29d584324f6efc1fe00b1a346d793e
Nolan Kantner <94024607+nolankantner@users.noreply.github.com>
2026-01-18 18:39:18 -0800

    Add files via upload

---
+++ python/AdditionWorkBoardCoinAmount.py
@@ -0,0 +1,9 @@
+coins = [0,0,0,0,0,0,0,0,0,0] #index counts as coin number
+for x in range(0,1000): #change 1000 in lines 2 and 3 for higher digit boards (100 for tens place, 10000 for thousands place, etc.)
+    for y in range(0,1000):
+        for i in range(0,10):
+            if str(x).count(str(i)) + str(y).count(str(i)) + str(x+y).count(str(i)) > coins[i]:
+                coins[i] = str(x).count(str(i)) + str(y).count(str(i)) + str(x+y).count(str(i))
+coins[1] += 2 #adds 2 extra 1 coins for the 2 sliders
+for i in range(0,10):
+    print("Coin",str(i),"  Amount",coins[i])
